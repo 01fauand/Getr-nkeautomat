bfbdda070684c0f43b39f4d95f741606522462c0
01pirhan <35956755+01pirhan@users.noreply.github.com>
2018-03-20 10:44:00 +0100

    beginn
    first commit

---
+++ Getraenkeautomat/Getraenkeautomat/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Getraenkeautomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btngetraenk1 = new System.Windows.Forms.Button();
+            this.btngetraenk2 = new System.Windows.Forms.Button();
+            this.btngetraenk3 = new System.Windows.Forms.Button();
+            this.btngetraenk4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(242, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 446);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btngetraenk1
+            // 
+            this.btngetraenk1.Location = new System.Drawing.Point(96, 71);
+            this.btngetraenk1.Name = "btngetraenk1";
+            this.btngetraenk1.Size = new System.Drawing.Size(101, 41);
+            this.btngetraenk1.TabIndex = 1;
+            this.btngetraenk1.Text = "Coca Cola";
+            this.btngetraenk1.UseVisualStyleBackColor = true;
+            // 
+            // btngetraenk2
+            // 
+            this.btngetraenk2.Location = new System.Drawing.Point(96, 143);
+            this.btngetraenk2.Name = "btngetraenk2";
+            this.btngetraenk2.Size = new System.Drawing.Size(101, 41);
+            this.btngetraenk2.TabIndex = 2;
+            this.btngetraenk2.Text = "Fanta";
+            this.btngetraenk2.UseVisualStyleBackColor = true;
+            // 
+            // btngetraenk3
+            // 
+            this.btngetraenk3.Location = new System.Drawing.Point(96, 216);
+            this.btngetraenk3.Name = "btngetraenk3";
+            this.btngetraenk3.Size = new System.Drawing.Size(101, 41);
+            this.btngetraenk3.TabIndex = 3;
+            this.btngetraenk3.Text = "Eistee";
+            this.btngetraenk3.UseVisualStyleBackColor = true;
+            // 
+            // btngetraenk4
+            // 
+            this.btngetraenk4.Location = new System.Drawing.Point(96, 295);
+            this.btngetraenk4.Name = "btngetraenk4";
+            this.btngetraenk4.Size = new System.Drawing.Size(101, 41);
+            this.btngetraenk4.TabIndex = 4;
+            this.btngetraenk4.Text = "Bier";
+            this.btngetraenk4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 366);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Preis";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Anzahl";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(614, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 57);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "10cent";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(614, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 57);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "50cent";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(722, 71);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(74, 57);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "20cent";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(614, 230);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(74, 57);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "2Euro";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(722, 152);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(74, 57);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "1Euro";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(710, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Eingezahlter Betrag";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(615, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Restgeld";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 557);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btngetraenk4);
+            this.Controls.Add(this.btngetraenk3);
+            this.Controls.Add(this.btngetraenk2);
+            this.Controls.Add(this.btngetraenk1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btngetraenk1;
+        private System.Windows.Forms.Button btngetraenk2;
+        private System.Windows.Forms.Button btngetraenk3;
+        private System.Windows.Forms.Button btngetraenk4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Getraenkeautomat/Getraenkeautomat/GetraenkeAutomat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Getraenkeautomat
+{
+    class GetraenkeAutomat
+    {
+        private int getraenk1;
+        private int getraenk2;
+        private int getraenk3;
+        private int getraenk4;
+
+        public GetraenkeAutomat()
+        {
+            getraenk1 = 5;
+            getraenk2 = 5;
+            getraenk3 = 5;
+            getraenk4 = 5;
+        }
+
+
+    }
+}
